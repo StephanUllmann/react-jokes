4be5192b935d4750f381fe107d6a138364c1d072
stephan ullmann <stephan.ullmann@posteo.de>
2024-08-23 16:39:51 +0200

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,85 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [jokes, setJokes] = useState([])
-  const [err, setErr] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [showArr, setShowArr] = useState([])
+  const [jokes, setJokes] = useState([]);
+  const [err, setErr] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showArr, setShowArr] = useState([]);
 
-  const getJokes = async () => {
+  const getJokes = async (signal) => {
     try {
-      setLoading(true)
-      setErr(null)
-      const res = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist&amount=10")
-      const data = await res.json()
-      console.log(data)
-      if (data.error) throw Error(data.message)
-    
-      setLoading(false)
-      setJokes(data.jokes)
+      setLoading(true);
+      setErr(null);
+      const res = await fetch('https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist&amount=10', {
+        signal,
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.error) throw Error(data.message);
+
+      setLoading(false);
+      setJokes(data.jokes);
     } catch (error) {
-      setErr(error)
-      setLoading(false)
-      
+      setErr(error);
+      setLoading(false);
     }
-  }
+  };
   const handleNewJokes = (e) => {
-		e.target.blur();
-		window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-		getJokes();
-	};
+    const controller = new AbortController();
+
+    e.target.blur();
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    getJokes(controller.signal);
+  };
 
   useEffect(() => {
-    getJokes()
+    const controller = new AbortController();
+    getJokes(controller.signal);
     // fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,racist,sexist&amount=10").then(res => res.json()).then(data => console.log(data))
-  }, [])
+    return () => controller.abort();
+  }, []);
 
   return (
     <>
-     <h1>Jokes</h1>
+      <h1>Jokes</h1>
 
-     {err && <p>Something went wrong <br/>{err.message}</p>}
-     {loading && <p>Loading...</p>}
+      {err && (
+        <p>
+          Something went wrong <br />
+          {err.message}
+        </p>
+      )}
+      {loading && <p>Loading...</p>}
 
-     {jokes.length > 0 && jokes.map(joke => 
-      joke.type === "single" ? 
-      <><p key={joke.id}>{joke.joke}</p><hr/></> : 
-      <div key={joke.id} >
-        <p>{joke.setup}</p>
-        {showArr.includes(joke.id) 
-        ? <><p>{joke.delivery}</p>
-            <button 
-            onClick={() => setShowArr(prev => prev.filter(id => id !== joke.id))}>
-          Hide Delivery
-          </button></> 
-        : <button 
-            onClick={() => setShowArr(prev => [...prev, joke.id])}>
-          Show Delivery
-          </button>}
-        <hr/></div>)}
+      {jokes.length > 0 &&
+        jokes.map((joke) =>
+          joke.type === 'single' ? (
+            <>
+              <p key={joke.id}>{joke.joke}</p>
+              <hr />
+            </>
+          ) : (
+            <div key={joke.id}>
+              <p>{joke.setup}</p>
+              {showArr.includes(joke.id) ? (
+                <>
+                  <p>{joke.delivery}</p>
+                  <button onClick={() => setShowArr((prev) => prev.filter((id) => id !== joke.id))}>
+                    Hide Delivery
+                  </button>
+                </>
+              ) : (
+                <button onClick={() => setShowArr((prev) => [...prev, joke.id])}>Show Delivery</button>
+              )}
+              <hr />
+            </div>
+          )
+        )}
 
-        <button onClick={(e) => handleNewJokes(e)}>Get new Jokes</button>
+      <button onClick={(e) => handleNewJokes(e)}>Get new Jokes</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
